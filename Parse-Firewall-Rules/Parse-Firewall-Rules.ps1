97203056e1d8250592c2385305a337c96f5a2e92
Kira <erwint@uci.edu>
2015-09-02 22:22:02 -0700

    Cleanup

--- Parse-Firewall-Rules/Parse-Firewall-Rules.ps1
+++ Parse-Firewall-Rules/Parse-Firewall-Rules.ps1
@@ -1,25 +1,16 @@
 ﻿Function Parse-Firewall-Rules($rawRules) {
 	[xml]$rules = [xml](Get-Content $rawRules)
-	[string]$saveRoot = File-Dialog($TRUE)
+	[string]$saveRoot = Dialog($TRUE)
 
-	$hosts = @()
-
-	foreach ($entry in $XMLHosts.entry) {
-		$aHost = [PSCustomObject]@{
-			Hostname = $entry.name
-			IP = $entry.'ip-netmask'
-			Location = $entry.tag.member
-			Description = $entry.description
-		}
-		$hosts += $aHost
-	}
-
+	# Walk the XML tree from the Panorama output.
+	# We are looking for the ruleset groups.
 	$ruleGroups = $rules.config.devices.entry.'device-group'.entry
 	[PSCustomObject]$securityRules = @()
 	foreach ($group in $ruleGroups) {
 		$XMLSecurityRules = $group.'pre-rulebase'.security.rules.entry
 		$XMLSecurityRules += $group.'post-rulebase'.security.rules.entry
 
+	   # Bundle each rule as a PowerScript object and sanitize some of the text output.
 	   [PSCustomObject]$securityRules = @()
 	   foreach ($rule in $XMLSecurityRules) {
 			$tmp = [PSCustomObject]@{
@@ -35,16 +26,21 @@
 		$securityRules += $tmp
 		}
 
+		# Save every ruleset as a CSV to the user specified folder.
+		# The default folder is wherever the script lives.
 		$securityRules | Export-CSV -PATH "$($saveRoot)\$($group.name).csv"
-		if ((Get-Random -Maximum 8) -eq 0) { [System.GC]::Collect() }
 	}
 }
 
+# This is just a fluffed-up string trim().
 Function Clean-String($str) {
 	return ($str -creplace '(?m)^\s*\r?\n', '').Trim()
 }
 
-Function File-Dialog($save) {
+# Convienence function to load the open file and save folder dialogs.
+# 
+# Function accepts input $TRUE or $FALSE; $TRUE will invoke the folder dialog.
+Function Dialog($save) {
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 	if ($save -eq $TRUE) {
 		$dialog = New-Object System.Windows.Forms.FolderBrowserDialog
@@ -59,6 +55,4 @@ Function File-Dialog($save) {
 	}		
 }
 
-
-#File-Dialog($FALSE)
-Parse-Firewall-Rules(File-Dialog($FALSE))
+Parse-Firewall-Rules(Dialog($FALSE))
